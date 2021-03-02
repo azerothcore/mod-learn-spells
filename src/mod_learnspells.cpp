3252fe1cae7f90bd157e6c64716e21b813cf0928
Vitaliy <mitradis@gmail.com>
2021-03-02 10:30:33 +0300

    Update mod_learnspells.cpp

--- src/mod_learnspells.cpp
+++ src/mod_learnspells.cpp
@@ -330,16 +330,6 @@ class LearnAllSpellsWorld : public WorldScript
     {
         if (!reload)
         {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/mod_learnspells.conf";
-#ifdef WIN32
-            cfg_file = "mod_learnspells.conf";
-#endif
-            std::string cfg_def_file = cfg_file + ".dist";
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-
-            sConfigMgr->LoadMore(cfg_file.c_str());
-
             MaxLevel = sConfigMgr->GetIntDefault("MaxLevel", 80);
         }
     }
